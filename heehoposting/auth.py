9bcbc515030a40b02d90da10b6d0e7806164cff4
jackson <jackson@heehoposting.xyz>
2022-05-01 04:19:47 -0400

    fixed auth.py and added file for register & login

--- heehoposting/auth.py
+++ heehoposting/auth.py
@@ -26,28 +26,28 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        db = get_db
+        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
-        if not password:
+        elif not password:
             error = 'Password is required.'
 
         if error is None:
             try:
                 db.execute(
-                    'INSERT INTO user (username, password) VALUES (?, ?)',
-                    (username, generate_password_hash(password))
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
-                error = f'User {username} is already registered.'
+                error = f"User {username} is already registered."
             else:
-                return redirect(url_for('auth.login'))
-        
+                return redirect(url_for("auth.login"))
+
         flash(error)
-    
+
     return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
@@ -63,15 +63,14 @@ def login():
 
         if user is None:
             error = 'Incorrect username.'
-
-        if not check_password_hash(user['password'], password):
+        elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
         if error is None:
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
-        
+
         flash(error)
 
     return render_template('auth/login.html')
