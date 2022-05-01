1a03362c9fbc74a01daa0e1b6812b34834d4b0b2
jackson <jackson@heehoposting.xyz>
2022-05-01 04:56:26 -0400

    added build/lib files, and more goodies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flaskr',
+    name='heehoposting',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
