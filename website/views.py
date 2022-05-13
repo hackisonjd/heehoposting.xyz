32aa0bb50502791d923e2d6f29d714d08fe33998
jackson <jackson@heehoposting.xyz>
2022-05-13 02:54:10 -0400

    added encoding for utf-8

--- website/views.py
+++ website/views.py
@@ -3,11 +3,11 @@ Valid views (or webpages) for Flask website.
 '''
 import glob
 import os.path
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 import markdown
 from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import check_password_hash
 from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 bp = Blueprint('views', __name__)
@@ -22,7 +22,6 @@ def dict_files():
             # return only the capitalized version of the basename of the path (w/o extension)
             file_key = (os.path.splitext(os.path.basename(file))[0]).capitalize()
             files[file_key] = ('views.' + file_key.lower())
-        
     return files
 
 navbar = dict_files()
@@ -32,8 +31,8 @@ def index():
     '''
     Homepage for the website.
     '''
-    with open('website/static/index.txt', 'r') as f:
-        text = f.read()
+    with open('website/static/index.txt', 'r', encoding="utf-8") as file:
+        text = file.read()
     text = markdown.markdown(text)
     return render_template('index.html', navbar=navbar, user=current_user,  text=text)
 
@@ -43,8 +42,8 @@ def about():
     Information about me, this is the portfolio bit.
     '''
 
-    with open('website/static/bio.txt', 'r') as f:
-        bio = f.read()
+    with open('website/static/bio.txt', 'r', encoding="utf-8") as file:
+        bio = file.read()
     bio = markdown.markdown(bio)
     print(bio)
 
@@ -55,35 +54,35 @@ def contact():
     '''
     Displays contact information on how to reach me directly.
     '''
-    with open('website/static/contact.txt', 'r') as f:
-        contact = f.read()
-    contact = markdown.markdown(contact)
-    return render_template('contact.html', navbar=navbar, contact=contact, user=current_user)
+    with open('website/static/contact.txt', 'r', encoding="utf-8") as file:
+        con = file.read()
+    con = markdown.markdown(contact)
+    return render_template('contact.html', navbar=navbar, contact=con, user=current_user)
 
 @bp.route('/links')
 def links():
     '''
     Displays useful links that helped me make this website in addition to other things.
     '''
-    with open('website/static/links.txt', 'r') as f:
-        links = f.read()
-    links = markdown.markdown(links)
-    return render_template('links.html', navbar=navbar, links=links, user=current_user)
+    with open('website/static/links.txt', 'r', encoding="utf-8") as file:
+        link = file.read()
+    link = markdown.markdown(link)
+    return render_template('links.html', navbar=navbar, links=link, user=current_user)
 
 @bp.route('/login', methods=['POST', 'GET'])
 def login():
+    '''
+    This method handles the login page for accounts, which is just my personal account for now.
+    '''
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        
         user = User.query.filter_by(username=username).first()
         if user:
             if check_password_hash(user.password, password):
                 login_user(user, remember=True)
                 return redirect(url_for('views.mastermind'))
-            else:
-                print('wrong')
-                flash('Wrong password!!', category='error')
+
     return render_template('/admin/login.html', user=current_user)
 
 @bp.route('/mastermind', methods=['POST', 'GET'])
